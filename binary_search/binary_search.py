aa9abdd68075dd0051bbab0e387fb2e6743d3d46
nayon117 <hasibul.nayon1@gmail.com>
2025-11-09 02:45:24 +0600

    solve binary search all problems

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -12,3 +12,5 @@ class Solution:
             else:
                 right = mid - 1
         return -1
+
+
---
+++ binary_search/koko_eating_bananas.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+       l, r = 1, max(piles)
+       res = r
+
+       while l <= r:
+           mid = (l + r) // 2
+           hours = sum((pile + mid - 1) // mid for pile in piles)
+
+           if hours <= h:
+               res = mid
+               r = mid - 1
+           else:
+               l = mid + 1
+       return res
+      
